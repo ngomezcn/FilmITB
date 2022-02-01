2c9a5f98f7463894d1dc1062176a373a89054e70
naim.gomez.7e5 <naim.gomez.7e5@itb.cat>
2022-02-01 12:54:42 +0100

    d

--- src/cat/itb/naimgomez7e5/m03/uf2/filmITB/model/User.kt
+++
@@ -1,9 +0,0 @@
-package cat.itb.naimgomez7e5.m03.uf2.filmITB.model
-
-data class User(
-    val id: Int,
-    val name: String,
-    val lastName: String,
-    val age: Int){
-    val likedFilms: List<String> = listOf()
-}
