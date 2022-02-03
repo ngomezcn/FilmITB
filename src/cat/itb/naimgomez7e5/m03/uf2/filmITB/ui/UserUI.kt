40999b58e4b8bb1554ca7a8adec76a5c00749298
ngomezKS <naimgomezcn@gmail.com>
2022-02-03 10:22:28 +0100

    .

--- src/cat/itb/naimgomez7e5/m03/uf2/filmITB/ui/UserUI.kt
+++ src/cat/itb/naimgomez7e5/m03/uf2/filmITB/ui/UserUI.kt
@@ -2,7 +2,6 @@ package cat.itb.naimgomez7e5.m03.uf2.filmITB.ui
 
 class UserUI {
 
-
     fun showMenu()
     {
         println("Users:")
@@ -15,7 +14,20 @@ class UserUI {
         println("7: Show statistics")
         println("0: Return to main menu")
 
+        when (AppState().inputInt()) {
+            1 -> addUser()
+            // TODO: Pon aqui las funciones que vayas creando
 
-        AppState().inputInt();
+            0 -> UI().showMainMenu();
+            else -> {
+                UI().showMainMenu();
+            }
+        }
+
+    }
+
+    fun addUser()
+    {
+        // Mete las opciones que creas
     }
 }
