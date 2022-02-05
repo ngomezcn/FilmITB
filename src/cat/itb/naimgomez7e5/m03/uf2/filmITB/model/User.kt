e4fe78a59cf7becdda716a97b5674409b367797d
ngomezKS <naimgomezcn@gmail.com>
2022-02-05 22:22:29 +0100

    d

--- src/cat/itb/naimgomez7e5/m03/uf2/filmITB/model/User.kt
+++
@@ -1,6 +0,0 @@
-package cat.itb.naimgomez7e5.m03.uf2.filmITB.model
-
-data class User(var name: String, var lastName: String, var age: Int)
-{
-    val likedFilms: List<String> = listOf()
-}
--- src/cat/itb/naimgomez7e5/m03/uf2/filmITB/ui/FilmUI.kt
+++
@@ -1,8 +0,0 @@
-package cat.itb.naimgomez7e5.m03.uf2.filmITB.ui
-
-class FilmUI {
-    fun showMenu()
-    {
-        println()
-    }
-}
