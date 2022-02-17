1d29a21f031d3d25bd4e0eaa7c232520e3ebd605
marti.garcia.7e4 <marti.garcia.7e4@itb.cat>
2022-02-17 11:53:30 +0100

    d'

--- src/main/kotlin/cat/itb/naimgomez7e5/m03/uf2/filmITB/test/FilmItbTest.kt
+++ src/main/kotlin/cat/itb/naimgomez7e5/m03/uf2/filmITB/test/FilmItbTest.kt
@@ -1,110 +1,126 @@
 package cat.itb.naimgomez7e5.m03.uf2.filmITB.test
 
-import org.junit.jupiter.api.Assertions.*
+import cat.itb.naimgomez7e5.m03.uf2.filmITB.model.*
+import cat.itb.naimgomez7e5.m03.uf2.filmITB.model.FilmItb
+import cat.itb.naimgomez7e5.m03.uf2.filmITB.model.User
+import cat.itb.naimgomez7e5.m03.uf2.filmITB.ui.AppState
+import org.junit.Test
+import org.junit.jupiter.api.BeforeEach
 
 internal class FilmItbTest {
 
-    @org.junit.jupiter.api.BeforeEach
+    val appState = AppState();
+
+    @BeforeEach
     fun setUp() {
+        appState.filmItb.films.add(Film("Avatar", "James Cameron", "Sci-Fi", "Sam Worthington", "...", 7, 145, 5343))
+        appState.filmItb.films.add(Film("Shrek", "Carl Peter", "Fantasy", "Elijah Wood", "...", 13, 222, 91235))
+        appState.filmItb.users.add(User("James", "Smith",12))
+        appState.filmItb.users.add(User("Robert", "Taylor",25))
+
+        AppState.currentUser = User("Michel", "Trevor", 45)
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun getFilms() {
+
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun getFilmsURL() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun getUsers() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun getUsersURL() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun loadData() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun deleteFilm() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun addFilm() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun watchedFilms() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun displayWatchedFilms() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun addFavorites() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun showFavorites() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun likesPerFilm() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun addUser() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun changeName() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun changeLastName() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun changeAge() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun deleteUser() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun getUserByName() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun searchFilmByTitle() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun searchDirectorByDirector() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun searchFilmByMainActor() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun searchFilmByMainGenere() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun searchFilmByDuration() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun searchFilmByNotWatched() {
+
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun searchFilmByRecomended() {
+
     }
 }
