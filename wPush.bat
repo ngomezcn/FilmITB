c8bec57ccb4f8b09eb6729d0a392dd314457a53f
ngomezcn <naimgomezcn@gmail.com>
2022-03-17 19:25:00 +0100

    .

--- wPush.bat
+++
@@ -1,8 +0,0 @@
-git add -A
-git commit -m "-"
-
-git remote add github "https://gitlab.com/naim.gomez.7e5/projecte-uf2-garciamarti-gomeznaim"
-git push origin
-
-git remote add github "https://github.com/ngomezcn/FilmITB"
-git push github
